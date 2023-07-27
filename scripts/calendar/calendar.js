02ba383b7f470ffb4462fc51b19ce563637b78a2
martynsvadim <martynenkovadim3@gmail.com>
2023-07-27 20:36:40 +0300

    project-js

---
+++ scripts/calendar/calendar.js
@@ -0,0 +1,26 @@
+import { getItem } from '../common/storage.js';
+import { generateWeekRange } from '../common/time.utils.js';
+import { renderEvents } from '../events/events.js';
+import { createNumbersArray } from '../common/createNumbersArray.js';
+
+const generateDay = () => {
+  // функция должна сгенерировать и вернуть разметку дня в виде строки
+  // разметка состоит из 24 часовых временных слотов (.calendar__time-slot)
+  const timeScale = document.querySelector('.calendar__time-scale');
+  for (let i = 0; i < 24; i++) {
+    const divElem = document.createElement('div');
+    const spanElem = document.createElement('span');
+    spanElem.classList.add('time-slot__time');
+    divElem.classList.add('time-slot');
+    timeScale.append(divElem);
+  }
+};
+generateDay();
+
+export const renderWeek = () => {
+  // функция должна сгенерировать разметку недели в виде строки и вставить ее на страницу (в .calendar__week)
+  // разметка недели состоит из 7 дней (.calendar__day) отображаемой недели
+  // массив дней, которые нужно отобразить, считаем ф-цией generateWeekRange на основе displayedWeekStart из storage
+  // каждый день должен содержать в дата атрибуте порядковый номер дня в месяце
+  // после того, как отрисовали всю сетку для отображаемой недели, нужно отобразить события этой недели с помощью renderEvents
+};
